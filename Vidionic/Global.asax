de52f3231153071ac6a680f72981a19848756d20
masroor <masroorh7@gmail.com>
2018-11-10 20:40:01 +0500

    Add project files.

---
+++ Vidionic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vidionic.MvcApplication" Language="C#" %>
---
+++ Vidionic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidionic.Startup))]
+namespace Vidionic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
