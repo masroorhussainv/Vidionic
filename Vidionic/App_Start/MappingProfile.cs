972de5e3592566a390cca2ce0abca03bb81c9503
masroor <masroorh7@gmail.com>
2018-12-03 01:35:58 +0500

    section 6, exercise remaining yet

---
+++ Vidionic/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidionic.Dtos;
+using Vidionic.Models;
+
+namespace Vidionic.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
